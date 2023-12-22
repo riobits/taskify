a2362be15869fb2f2d0a201376a2c688de0d62de
riobits <23579928+riobits@users.noreply.github.com>
2023-12-22 19:26:46 +0300

    Better emoji button style

--- src/pages/GroupTasksPage.jsx
+++ src/pages/GroupTasksPage.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import { useParams } from 'react-router-dom'
 import EmojiPicker from 'emoji-picker-react'
 import { SmilePlus } from 'lucide-react'
+import { getTasks } from '../utils/storage'
 import styles from './GroupTasksPage.module.css'
 
 const GroupTasksPage = () => {
+  const params = useParams()
+  const [tasks, setTasks] = useState(getTasks(params.groupId))
   const [isEmojiPickerVisible, setIsEmojiPickerVisible] = useState(false)
 
   const handleToggleEmojiPicker = () => {
@@ -14,8 +18,11 @@ const GroupTasksPage = () => {
     <div>
       <div className={styles['new-task']}>
         <div>
-          <button className={styles['emoji-button']}>
-            <SmilePlus size={18} onClick={handleToggleEmojiPicker} />
+          <button
+            className={styles['emoji-button']}
+            onClick={handleToggleEmojiPicker}
+          >
+            <SmilePlus size={18} />
           </button>
           <div className={styles['emoji-picker-container']}>
             {isEmojiPickerVisible && <EmojiPicker />}
@@ -27,6 +34,9 @@ const GroupTasksPage = () => {
         />
       </div>
       <p className={styles.note}>Press Enter to insert.</p>
+      {/* {tasks.map((task) => {
+        return <div key={task.name}>{task.name}</div>
+      })} */}
     </div>
   )
 }
