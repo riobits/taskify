ea4fc5e83a1a55c9629305283b716b0dd221d504
riobits <23579928+riobits@users.noreply.github.com>
2024-01-13 19:32:41 +0300

    Add footer

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles['footer']}>
+      © 2024 Taskify. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
